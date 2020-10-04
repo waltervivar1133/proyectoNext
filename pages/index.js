0907670e3c0f3116991c6ebbb7cfaef66511b5df
walter <waltervivar1133@gmail.com>
2020-10-04 16:49:45 -0500

    [ADD DETAILS-PRODUCT && MAP PRODUCTOS ]

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,50 @@
 import Layout from '../components/layouts/Layout'
 import React ,{useEffect , useState, useContext}from 'react';
 import {FirebaseContext} from '../firebase';
+//components
+import DetallesProducto from '../components/layouts/DetalleProducto'
+
 
 
 const Home= () => {
 
+const [productos , guardarProductos] = useState([]);
 
-  
+const { firebase } = useContext(FirebaseContext);
+
+useEffect(() => {
+  const obtenerProductos = () => {
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot)
+  }
+  obtenerProductos()
+}, [])
+
+function manejarSnapshot(snapshot){
+  const productos = snapshot.docs.map(doc =>{
+    return {
+      id: doc.id,
+      ...doc.data()
+    }
+  });
+
+  guardarProductos(productos)
+}
+
   return (
     <div>
       <Layout>
-        <h2>Inicio</h2>
+        <div className="listado-productos">
+            <div className="contenedor">
+              <ul className="bg-white">
+                  {productos.map(producto =>(
+                    <DetallesProducto
+                      key = {producto.id}
+                      producto = {producto}
+                    />
+                  ))}
+              </ul>
+            </div>
+        </div>
       </Layout>
      
      
